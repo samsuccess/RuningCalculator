b4951b8124caa2079883679c704dc494fe284ad3
samsuccess <samsuccess36@gmail.com>
2021-01-25 21:41:11 +0300

    Initial commit

---
+++ app/src/main/java/ru/example/runingcalculator/MainActivity.java
@@ -0,0 +1,23 @@
+package ru.example.runingcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button run = findViewById(R.id.but_run);
+        run.setOnClickListener(this);
+
+    }
+}
