14ae64e7bed917359b8c281559dc0b7cc48b1d9a
samsuccess <samsuccess36@gmail.com>
2021-01-25 22:34:36 +0300

    change1 homework5.1

--- app/src/main/java/ru/example/runingcalculator/MainActivity.java
+++ app/src/main/java/ru/example/runingcalculator/MainActivity.java
@@ -1,23 +1,25 @@
 package ru.example.runingcalculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity<v> extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public void onClick(View v) {
         Button run = findViewById(R.id.but_run);
-        run.setOnClickListener(this);
+        run.setOnClickListener(v -> {
+            Uri uri = Uri.parse(getString(R.string.intent_uri));
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
 
+        });
     }
 }
